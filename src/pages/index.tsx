7915a6caf0716eb14025193428554570fbb5cfd9
ShenyiCui <shenyicui@outlook.com>
2024-03-26 23:42:46 +0800

    :sparkles: feat: add skeleton loading for friend component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,15 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import {
   Bars3CenterLeftIcon,
   BarsArrowUpIcon,
   UsersIcon,
 } from "@heroicons/react/16/solid";
+
+type Friend = {
+  id: string;
+  name: string;
+  amount: number;
+};
 
 const tabs = [{ name: "FRIENDS" }, { name: "GROUPS" }, { name: "ACTIVITY" }];
 const friends = [
@@ -57,14 +63,62 @@ const friends = [
     name: "Jenny Lim",
     amount: 0,
   },
-];
+] as Friend[];
 
 function classNames(...classes: string[]) {
   return classes.filter(Boolean).join(" ");
 }
 
+const LoadingSkeleton = () => {
+  const skeleton = Array.from(Array(5).keys()).map((i) => `skeleton-${i}`);
+  return (
+    <>
+      {skeleton.map((s) => (
+        <li
+          key={s}
+          className="col-span-1 flex rounded-md shadow-sm animate-pulse"
+        >
+          <div className="flex h-[58px] w-16 flex-shrink-0 items-center justify-center rounded-l-md text-white bg-slate-200" />
+          <div className="flex flex-1 items-center justify-between truncate rounded-r-md border-b border-r border-t border-gray-200 bg-white">
+            <div className="flex-1 truncate px-4 py-2 text-sm">
+              <div className="bg-slate-200 h-3 w-11 mb-2" />
+              <div className="bg-slate-200 h-3 w-24" />
+            </div>
+            <div className="flex-shrink-0 pr-2">
+              <div className="bg-slate-200 rounded-full h-6 w-6" />
+            </div>
+          </div>
+        </li>
+      ))}
+    </>
+  );
+};
+
 const Home = () => {
   const [currentTab, setCurrentTab] = useState("FRIENDS");
+  const [friendsData, setFriendsData] = useState<Friend[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const getFriendsData = async () => {
+    console.log("fetching data");
+    try {
+      setIsLoading(true);
+      const fetchedData = await new Promise<Friend[]>((resolve) => {
+        setTimeout(() => {
+          resolve(friends);
+        }, 4000);
+      });
+      setFriendsData(fetchedData);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getFriendsData();
+  }, []);
 
   const handleTabChange = (tabName: string) => {
     setCurrentTab(tabName);
@@ -159,59 +213,63 @@ const Home = () => {
         </div>
 
         <div className="flex flex-col h-full gap-4 overflow-y-auto pb-4">
-          {friends.map((friend) => {
-            const isSettledUp = friend.amount === 0;
-            const isOwed = friend.amount > 0;
-            const bgColor = isSettledUp
-              ? "bg-gray-500"
-              : isOwed
-              ? "bg-greenBg"
-              : "bg-redBg";
-            const money = isSettledUp
-              ? "All settled up"
-              : isOwed
-              ? `You are owed $${friend.amount}`
-              : `You owe $${-friend.amount}`;
+          {!isLoading &&
+            friendsData.map((friend) => {
+              const isSettledUp = friend.amount === 0;
+              const isOwed = friend.amount > 0;
+              const bgColor = isSettledUp
+                ? "bg-gray-500"
+                : isOwed
+                ? "bg-greenBg"
+                : "bg-redBg";
+              const money = isSettledUp
+                ? "All settled up"
+                : isOwed
+                ? `You are owed $${friend.amount}`
+                : `You owe $${-friend.amount}`;
+              const initals = friend.name
+                .split(" ")
+                .map((n) => n[0])
+                .join("");
 
-            return (
-              <li
-                key={friend.id}
-                className="col-span-1 flex rounded-md shadow-sm"
-              >
-                <div
-                  className={classNames(
-                    "flex w-16 flex-shrink-0 items-center justify-center rounded-l-md text-sm font-medium text-white",
-                    bgColor
-                  )}
+              return (
+                <li
+                  key={friend.id}
+                  className="col-span-1 flex rounded-md shadow-sm"
                 >
-                  {friend.name
-                    .split(" ")
-                    .map((name) => name[0])
-                    .join("")}
-                </div>
-                <div className="flex flex-1 items-center justify-between truncate rounded-r-md border-b border-r border-t border-gray-200 bg-white">
-                  <div className="flex-1 truncate px-4 py-2 text-sm">
-                    <a className="font-medium text-gray-900 hover:text-gray-600">
-                      {friend.name}
-                    </a>
-                    <p className="text-gray-500">{money}</p>
+                  <div
+                    className={classNames(
+                      "flex w-16 flex-shrink-0 items-center justify-center rounded-l-md text-sm font-medium text-white",
+                      bgColor
+                    )}
+                  >
+                    {initals}
                   </div>
-                  <div className="flex-shrink-0 pr-2">
-                    <button
-                      type="button"
-                      className="inline-flex h-8 w-8 items-center justify-center rounded-full bg-transparent bg-white text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                    >
-                      <span className="sr-only">Open options</span>
-                      <Bars3CenterLeftIcon
-                        className="h-5 w-5"
-                        aria-hidden="true"
-                      />
-                    </button>
+                  <div className="flex flex-1 items-center justify-between truncate rounded-r-md border-b border-r border-t border-gray-200 bg-white">
+                    <div className="flex-1 truncate px-4 py-2 text-sm">
+                      <a className="font-medium text-gray-900 hover:text-gray-600">
+                        {friend.name}
+                      </a>
+                      <p className="text-gray-500">{money}</p>
+                    </div>
+                    <div className="flex-shrink-0 pr-2">
+                      <button
+                        type="button"
+                        className="inline-flex h-8 w-8 items-center justify-center rounded-full bg-transparent bg-white text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                      >
+                        <span className="sr-only">Open options</span>
+                        <Bars3CenterLeftIcon
+                          className="h-5 w-5"
+                          aria-hidden="true"
+                        />
+                      </button>
+                    </div>
                   </div>
-                </div>
-              </li>
-            );
-          })}
+                </li>
+              );
+            })}
+
+          {isLoading && <LoadingSkeleton />}
         </div>
       </div>
     </div>
